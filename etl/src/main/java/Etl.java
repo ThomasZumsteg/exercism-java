2fbb4aa48144f341e79bb7b1b31d1fbae9fa683a
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-10-21 14:23:18 -0700

    Etl compiles

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,2 +1,16 @@
-public Etl {
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+
+public class Etl {
+    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+        final Map<String, Integer> result = new HashMap<String, Integer>();
+        result.put("a", 1);
+        result.put("e", 1);
+        result.put("i", 1);
+        result.put("o", 1);
+        result.put("u", 1);
+        return result;
+
+    }
 }
