ffcb3c1aa56fe88b8d43794cfb08cc3e8a6067f3
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-10-22 10:40:14 -0700

    First iteration etl

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -3,14 +3,13 @@ import java.util.List;
 import java.util.HashMap;
 
 public class Etl {
-    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        final Map<String, Integer> result = new HashMap<String, Integer>();
-        result.put("a", 1);
-        result.put("e", 1);
-        result.put("i", 1);
-        result.put("o", 1);
-        result.put("u", 1);
-        return result;
-
+    public Map<String, Integer> transform(Map<Integer, List<String>> oldMap) {
+        final Map<String, Integer> newMap = new HashMap<String, Integer>();
+        for(Integer key: oldMap.keySet()) {
+            for(String val: oldMap.get(key)) {
+                newMap.put(val.toLowerCase(), key);
+            }
+        }
+        return newMap;
     }
 }
