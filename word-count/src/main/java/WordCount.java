5a49c759d308933d16f588e2e0ca84b4e463129f
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-10-24 15:54:22 -0700

    First iteration word-count

---
+++ word-count/src/main/java/WordCount.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCount {
+    public Map<String, Integer> Phrase(String sentence) {
+        Map<String, Integer> wordCounts = new HashMap<String, Integer>();
+        String[] words = sentence.toLowerCase().replaceAll("[^a-z0-9 ]", "").split("\\s+");
+        for(String word: words) {
+            Integer c = wordCounts.containsKey(word) ? wordCounts.get(word) : 0;
+            wordCounts.put(word, ++c);
+        }
+        return wordCounts;
+    }
+}
