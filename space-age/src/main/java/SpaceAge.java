59964a637a1d7cbe33efb99e1ef4555504ab0567
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-11-02 09:42:14 -0800

    Update to space-age

--- space-age/src/main/java/SpaceAge.java
+++ space-age/src/main/java/SpaceAge.java
@@ -14,34 +14,34 @@ public class SpaceAge {
     }
 
     public double onEarth() {
-        return this.seconds / (31557600 * 1.0);
+        return this.seconds / 31557600.0;
     }
 
     public double onMercury() {
-        return this.seconds / (31557600 * 0.2408467);
+        return onEarth() / 0.2408467;
     }
 
     public double onVenus() {
-        return this.seconds / (31557600 * 0.61519726);
+        return onEarth() / 0.61519726;
     }
 
     public double onMars() {
-        return this.seconds / (31557600 * 1.8808158);
+        return onEarth() / 1.8808158;
     }
 
     public double onJupiter() {
-        return this.seconds / (31557600 * 11.862615);
+        return onEarth() / 11.862615;
     }
 
     public double onSaturn() {
-        return this.seconds / (31557600 * 29.447498);
+        return onEarth() / 29.447498;
     }
 
     public double onUranus() {
-        return this.seconds / (31557600 * 84.016846);
+        return onEarth() / 84.016846;
     }
 
     public double onNeptune() {
-        return this.seconds / (31557600 * 164.79132);
+        return onEarth() / 164.79132;
     }
 }
