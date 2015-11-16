ead62f338b7ed8a4a4bc56f24fdab46af1289b6d
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-11-16 10:32:51 -0800

    First iteration sieve

---
+++ sieve/src/main/java/Sieve.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Sieve {
+    private boolean[] sieve;
+
+    public Sieve(int limit) {
+        sieve = new boolean[limit];
+    }
+
+    public List<Integer> getPrimes() {
+        List<Integer> primes = new ArrayList<>();
+        if(2 <= sieve.length ) primes.add(2);
+        for(int i = 3; i < sieve.length; i += 2) {
+            if(!sieve[i]) {
+                primes.add(i);
+                for(int m = i * i; m < sieve.length; m += i)
+                    sieve[m] = true;
+            }
+        }
+        return primes;
+    }
+}
