feeb6f8f5094db6dbf4a3569f5d8b794b59b629d
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-11-17 09:41:46 -0800

    Updated etl to use streams

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -4,12 +4,11 @@ import java.util.HashMap;
 
 public class Etl {
     public Map<String, Integer> transform(Map<Integer, List<String>> oldMap) {
-        final Map<String, Integer> newMap = new HashMap<String, Integer>();
-        for(Map.Entry<Integer, List<String>> entry: oldMap.entrySet()) {
-            for(String val: entry.getValue()) {
-                newMap.put(val.toLowerCase(), entry.getKey());
-            }
-        }
+        Map<String, Integer> newMap = new HashMap<>();
+        oldMap.entrySet().stream()
+            .forEach(e -> e.getValue().stream()
+                .forEach(v -> newMap.put(v.toLowerCase(), e.getKey()))
+            );
         return newMap;
     }
 }
